a44d901e44f79bcf61ee57d415c0711a2f2aef9a
Prithvi Prakash <Sunny@Prithvis-MacBook-Pro-2.local>
2019-08-06 20:33:43 +0530

    fixed recrypt function

--- services/switchpoint/switchPointEncryption.py
+++ services/switchpoint/switchPointEncryption.py
@@ -7,7 +7,6 @@ from itertools import cycle
 def constructKey(seedValue):
     np.random.seed(int(seedValue))
     x = np.random.randint(1000000, 9999999)
-    print("X: {}".format(x))
     return x
 
 
@@ -19,22 +18,8 @@ def reCrypt(cipherText, timeStamp):
     oldKey = constructKey(timeStamp)
     now = int(time.time())
     newKey = constructKey(now)
-    # diffKey = oldKey^newKey
     diffKey = encrypt(str(oldKey), str(newKey))
-    oldCipher = encrypt(cipherText, oldKey)
-    diffCipher = encrypt(oldCipher, diffKey)
-    plain = encrypt(diffCipher, newKey)
-    print("Old Key: {}".format(oldKey))
-    print("Diff Key: {}".format(diffKey))
-    print("LDK {}".format(len(diffKey)))
-    print("New Key: {}".format(newKey))
-
-
-    print("Cipher: {}".format(cipherText))
-    print("Old Cipher: {}".format(oldCipher))
-    print("Diff Cipher: {}".format(diffCipher))
-    print("Plain: {}".format(plain))
-    # print(plain==cipherText)
+    diffCipher = encrypt(cipherText, diffKey)
     returnObject = {}
     returnObject["updated"] = now
     returnObject["password"] = diffCipher
